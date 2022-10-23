53d11d514b77d05b753d3c8e0669e67c69d191c8
adriellison <adriellisonki@gmail.com>
2022-10-23 20:25:39 -0300

    verificar linhas do inicio de cada matriz

--- impasses.py
+++ impasses.py
@@ -1,4 +1,5 @@
 # Funções
+from concurrent.futures import process
 from configs.simple import *
 
 def verificarArquivo():
@@ -11,9 +12,41 @@ def verificarArquivo():
 		print('O arquivo não pode ser aberto.\nPor favor, verifique se o arquivo existe e está fechado.')
 	press()
 
-def lerArquivo():
+def verificaRecursos():
 	file = open('entrada.txt', 'r')
-	saida = file.read()
+	linha = file.readline()
 	file.close()
-	print(saida)
-	press()
+	lista = linha.split(" ")
+	return lista
+
+def imprimeRecursos():
+	print(f"Números de processos: {verificaRecursos()[0]}")
+	print(f"Número de recursos: {verificaRecursos()[1]}")
+	
+def allFile():
+	file = open('entrada.txt', 'r')
+	linhas = file.readlines()
+	file.close()
+	return linhas
+
+def verificarLinhas():
+	processos = int(verificaRecursos()[0])
+	recursos = int(verificaRecursos()[1])
+
+	print(f"Números de processos: {verificaRecursos()[0]}")
+	print(f"Número de recursos: {verificaRecursos()[1]}")
+	
+	linhas = allFile()
+	linhas = [ x for x in linhas if x != '\n' ]
+	# print(linhas)
+	# matriz1 = linhas[3]
+	# print(linhas[3])
+	primeiroArray = 3
+	segundoArray = primeiroArray + processos
+	print(linhas[primeiroArray])
+	print(linhas[segundoArray])
+	
+	for j in linhas
+
+
+verificarLinhas()
